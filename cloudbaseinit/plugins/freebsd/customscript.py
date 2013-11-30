cbe24737b1b13777e84a2e11f5d537dd64b34a36
Iblis Lin <e196819@hotmail.com>
2013-12-01 02:20:35 +0800

    1. Using /tmp when /root/scripts exists and not a dir 2. Add postfix to file by pid 3. Remove the script after executed

--- cloudbaseinit/plugins/freebsd/customscript.py
+++ cloudbaseinit/plugins/freebsd/customscript.py
@@ -14,15 +14,19 @@ class CustomScriptPlugin(base.BasePlugin):
     def execute(self, service):
         user_data = service.get_user_data('openstack')
         script_dir = '/root/scripts'
-        file_path = script_dir + '/CustomScript'
 
-        if ( !os.path.isdir(script_dir) ):
+        if ( !os.path.exists(script_dir) ):
             os.mkdir(script_dir, 0755)
+        else
+            if( !os.path.isdir(script_dir) ):
+                script_dir = '/tmp'
 
+        file_path = '%s/CustomScript.%d' % (script_dir, os.getpid())
         script_file = open(file_path, 'w')
         script_file.write(user_data)
         script_file.close()
         os.chmod(file_path, 0700)
         self._call_shell(file_path)
+        os.remove(file_path)
 
         return (base.PLUGIN_EXECUTION_DONE, False)
