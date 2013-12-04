291b3984135c95eccee11549c0eb2a63b658195d
Iblis Lin <e196819@hotmail.com>
2013-12-04 14:46:01 +0800

    Rename customscript.py to user_data.py and add warning msg for creating dir failed.

--- cloudbaseinit/plugins/freebsd/userdata.py
+++ cloudbaseinit/plugins/freebsd/userdata.py
@@ -4,29 +4,33 @@ from cloudbaseinit.openstack.common import log as logging
 
 import subprocess
 import os
+import tempfile
+import uuid
 
 LOG = logging.getLogger(__name__)
 
-class CustomScriptPlugin(base.BasePlugin):
+class UserDataPlugin(base.BasePlugin):
     def _call_shell(self, cmd):
         return subprocess.check_call(cmd, stderr=subprocess.STDOUT, shell=True)
 
     def execute(self, service):
         user_data = service.get_user_data('openstack')
-        script_dir = '/root/scripts'
+        script_dir = '/root/.bsd-cloudinit'
 
         if ( !os.path.exists(script_dir) ):
             os.mkdir(script_dir, 0755)
         else
             if( !os.path.isdir(script_dir) ):
-                script_dir = '/tmp'
+                LOG.warn('Cannot create dir: ' + script_dir + '.'
+                        'Using' + tempfile.gettempdir() + ' instead.')
+                script_dir = tempfile.gettempdir()
 
-        file_path = '%s/CustomScript.%d' % (script_dir, os.getpid())
+        file_path = '%s/UserData.%d' % (script_dir, uuid.uuid4())
         script_file = open(file_path, 'w')
         script_file.write(user_data)
         script_file.close()
-        os.chmod(file_path, 0700)
-        self._call_shell(file_path)
-        os.remove(file_path)
+        #os.chmod(file_path, 0700)
+        #self._call_shell(file_path)
+        #os.remove(file_path)
 
         return (base.PLUGIN_EXECUTION_DONE, False)
