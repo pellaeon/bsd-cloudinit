54b8bdc48fed1e663163bd594cad29507439e542
Pellaeon Lin <nfsmwlin@gmail.com>
2014-10-31 22:55:52 +0800

    Remove windows-specific parts of SetHostNamePlugin

--- cloudbaseinit/plugins/freebsd/sethostname.py
+++ cloudbaseinit/plugins/freebsd/sethostname.py
@@ -1,6 +1,7 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
 # Copyright 2012 Cloudbase Solutions Srl
+# Copyright 2014 Pellaeon Lin <pellaeon@hs.ntnu.edu.tw>
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -14,28 +15,16 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import platform
-import re
-
 from oslo.config import cfg
 
 from cloudbaseinit.openstack.common import log as logging
 from cloudbaseinit.osutils import factory as osutils_factory
 from cloudbaseinit.plugins import base
 
-opts = [
-    cfg.BoolOpt('netbios_host_name_compatibility', default=True,
-                help='Truncates the hostname to 15 characters for Netbios '
-                     'compatibility'),
-]
-
 CONF = cfg.CONF
 CONF.register_opts(opts)
 
 LOG = logging.getLogger(__name__)
-
-NETBIOS_HOST_NAME_MAX_LEN = 15
-
 
 class SetHostNamePlugin(base.BasePlugin):
     def execute(self, service, shared_data):
@@ -44,28 +33,8 @@ class SetHostNamePlugin(base.BasePlugin):
         metadata_host_name = service.get_host_name()
         if not metadata_host_name:
             LOG.debug('Hostname not found in metadata')
-            return (base.PLUGIN_EXECUTION_DONE, False)
 
-        metadata_host_name = metadata_host_name.split('.', 1)[0]
+        # In FreeBSD, hostname means FQDN, so no split is needed
+        osutils.set_host_name(metadata_host_name)
 
-        if (len(metadata_host_name) > NETBIOS_HOST_NAME_MAX_LEN and
-                CONF.netbios_host_name_compatibility):
-            new_host_name = metadata_host_name[:NETBIOS_HOST_NAME_MAX_LEN]
-            LOG.warn('Truncating host name for Netbios compatibility. '
-                     'Old name: %(metadata_host_name)s, new name: '
-                     '%(new_host_name)s' %
-                     {'metadata_host_name': metadata_host_name,
-                      'new_host_name': new_host_name})
-        else:
-            new_host_name = metadata_host_name
-
-        new_host_name = re.sub(r'-$', '0', new_host_name)
-        if platform.node().lower() == new_host_name.lower():
-            LOG.debug("Hostname already set to: %s" % new_host_name)
-            reboot_required = False
-        else:
-            LOG.info("Setting hostname: %s" % new_host_name)
-            osutils.set_host_name(new_host_name)
-            reboot_required = True
-
-        return (base.PLUGIN_EXECUTION_DONE, reboot_required)
+        return (base.PLUGIN_EXECUTION_DONE, False)
