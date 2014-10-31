26d1b0197a9f3b401fe3266002ea146bc86843b7
Pellaeon Lin <nfsmwlin@gmail.com>
2014-10-31 16:32:51 +0800

    Hardcode load osutils.freebsd

--- cloudbaseinit/osutils/factory.py
+++ cloudbaseinit/osutils/factory.py
@@ -16,12 +16,6 @@
 
 from cloudbaseinit.utils import classloader
 
-#TODO: fix for FreeBSD
 def get_os_utils():
-    osutils_class_paths = {
-        'nt': 'cloudbaseinit.osutils.windows.WindowsUtils',
-        'posix': 'cloudbaseinit.osutils.posix.PosixUtils'
-    }
-
     cl = classloader.ClassLoader()
-    return cl.load_class(osutils_class_paths[os.name])()
+    return cl.load_class('cloudbaseinit.osutils.freebsd.FreeBSDUtils')()
