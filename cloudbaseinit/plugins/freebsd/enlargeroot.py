cf8b8e4c181ccbe16ae03e5896b1624837a2014c
cao <cao@cao.vc>
2015-02-11 16:10:06 -0800

    Add ZFS support for FreeBSD's enlarge root

--- cloudbaseinit/plugins/freebsd/enlargeroot.py
+++ cloudbaseinit/plugins/freebsd/enlargeroot.py
@@ -2,6 +2,7 @@ from cloudbaseinit.osutils import factory as osutils_factory
 from cloudbaseinit.plugins import base
 from cloudbaseinit.openstack.common import log as logging
 
+import re
 import subprocess
 
 LOG = logging.getLogger(__name__)
@@ -10,10 +11,26 @@ class EnlargeRoot(base.BasePlugin):
     def _call_shell(self, cmd):
         return subprocess.check_call(cmd, stderr=subprocess.STDOUT, shell=True)
 
+    def _call_shell_output(self, cmd):
+        return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+
     def execute(self, service, shared_data):
         rootdisk = 'vtbd0'
+
+        # We might have swap before the root partition
+        gpart_output = self._call_shell_output('gpart show -p ' + rootdisk)
+        regex = rootdisk + r'p(?P<part>[0-9]+)\s+freebsd-(?P<fs>(u|z)fs)'
+        match = re.search(regex, gpart_output)
+        partition = match.group('part')
+        filesystem = match.group('fs')
+
         self._call_shell('gpart recover ' + rootdisk)
         self._call_shell('sysctl kern.geom.debugflags=16')
-        self._call_shell('gpart resize -i 2 ' + rootdisk)
-        self._call_shell('growfs -y /dev/' + rootdisk + 'p2')
+        self._call_shell('gpart resize -i ' + partition + ' ' + rootdisk)
+
+        if filesystem == 'ufs':
+            self._call_shell('growfs -y /dev/' + rootdisk + 'p' + partition)
+        elif filesystem == 'zfs':
+            self._call_shell('zpool set autoexpand=on zroot')
+            self._call_shell('zpool online -e zroot ' + rootdisk + 'p' + partition)
         return (base.PLUGIN_EXECUTION_DONE, False)
